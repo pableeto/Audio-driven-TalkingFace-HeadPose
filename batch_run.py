b7c54670e7207f5c024cb08d14d79e2d1358784c
Xiao Li <xili11@microsoft.com>
2020-07-13 18:57:23 +0800

    refactoring running script

--- batch_run.py
+++ batch_run.py
@@ -1,22 +1,47 @@
 import os
+from argparse import ArgumentParser
 
-n = 31 #person id
-gpu = 0
-audio = '03Fsi1831'
+# Argument parser.
+parser = ArgumentParser()
+# data related args.
+parser.add_argument('--video_id', type=int, default=0)
+parser.add_argument('--gpu_id', type=int, default=0)
+parser.add_argument('--audio_file', type=str, default='')
 
-## finetuning on a target person
-cmd1='cd Data/; python extract_frame1.py %d.mp4' % n
-os.system(cmd1)
 
-cmd2='cd Deep3DFaceReconstruction/; CUDA_VISIBLE_DEVICES=%d python demo_19news.py ../Data/%d' % (gpu,n)
-os.system(cmd2)
+if __name__ == "__main__":
+    # Parse argument
+    args = parser.parse_args()
+    n = args.video_id
+    gpu = args.gpu_id
+    audio_file = args.audio_file
+    _, name = os.path.split(audio_file)
+    audio_name, ext = os.path.splitext(name)
 
-cmd3='cd Audio/code; python train_19news_1.py %d %d' % (n,gpu)
-os.system(cmd3)
+    # convert to 25fps
+    cmd0 = f'ffmpeg -i ./Data/{n}.mp4 -r 25 -strict 2 ./Data/{n}.mp4'
+    os.system(cmd0)
 
-cmd4='cd render-to-video; python train_19news_1.py %d %d' % (n,gpu)
-os.system(cmd4)
+    # extract frames
+    cmd1 = f'cd Data/; python extract_frame1.py {n}.mp4'
+    os.system(cmd1)
 
-## test
-cmd5='cd Audio/code; python test_personalized.py %s %d %d' % (audio,n,gpu)
-os.system(cmd5)
+    # 3d recon
+    cmd2 = f'cd Deep3DFaceReconstruction/; CUDA_VISIBLE_DEVICES={gpu} python demo_19news.py ../Data/{n}'
+    os.system(cmd2)
+
+    # fine-tune audio
+    cmd3 = f'cd Audio/code; python train_19news_1.py {n} {gpu}' 
+    os.system(cmd3)
+
+    # fine-tune video
+    cmd4 = f'cd render-to-video; python train_19news_1.py {n} {gpu}'
+    os.system(cmd4)
+
+    # copy audio to corrsponded folder
+    cmd5 = f'cp {audio_file} ./Audio/audio'
+    os.system(cmd5)
+
+    # synthesis new results
+    cmd6 = f'cd Audio/code; python test_personalized.py {audio_name} {n} {gpu}'
+    os.system(cmd6)
